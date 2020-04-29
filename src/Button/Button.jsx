a6f9e582462efd0c8aa97d82b8ddd0f01bd18c3b
Mihai <mihai.rosca@thinslices.com>
2020-04-29 18:54:42 +0300

    chore(proptypes): add proptypes for button and typography

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -8,8 +8,14 @@ const Button = ({ onClick, title, ...props }) => (
 
 const buttonVariants = ['primary', 'outline', 'outline-alt', 'link']
 Button.propTypes = {
+  disabled: PropTypes.bool,
+  icon: PropTypes.shape({
+    name: PropTypes.string,
+    size: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  }),
   onClick: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
+  type: PropTypes.string,
   variant: PropTypes.oneOfType([
     PropTypes.oneOf(buttonVariants),
     PropTypes.objectOf(PropTypes.oneOf(buttonVariants))
--- src/Typography/Typography.jsx
+++ src/Typography/Typography.jsx
@@ -3,10 +3,26 @@ import PropTypes from 'prop-types'
 
 import { Typography as KogaioTypography } from '@ivoryio/kogaio'
 
-const Typography = ({ children, ...props }) => <KogaioTypography {...props}>{children}</KogaioTypography>
+const Typography = ({ children, ...props }) => (
+  <KogaioTypography {...props}>{children}</KogaioTypography>
+)
 
 Typography.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired
+  as: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  truncate: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.number,
+    PropTypes.string
+  ]),
+  variant: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.objectOf(PropTypes.string)
+  ])
+}
+
+Typography.defaultProps = {
+  as: 'div'
 }
 
 export default Typography
