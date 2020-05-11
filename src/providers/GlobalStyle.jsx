167b86d1d59c2b4f1c5885a1ee7a2e6cfd749ce3
Cezar Berea <berea.cezar@gmail.com>
2020-05-11 11:15:00 +0300

    Improve GlobalStyle

--- src/providers/GlobalStyle.jsx
+++ src/providers/GlobalStyle.jsx
@@ -5,9 +5,25 @@ export const GlobalStyle = createGlobalStyle`
 :root {
   background: ${themed('colors.background')};
   min-height: 100vh;
-  min-width: 100vw;
+  width: 100%;
   margin: 0;
   padding: 0;
 }
+
+body, html {
+  margin: 0;
+  padding: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+* {
+  box-sizing: border-box;
+}
+
 ${themed('root')}
 `
