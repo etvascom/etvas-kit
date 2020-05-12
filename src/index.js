ccd536173a0c42804a006a97b08486b728c2a13a
Cezar Berea <berea.cezar@gmail.com>
2020-05-12 10:59:09 +0300

    Remove duplicate imports

--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
 import styled from 'styled-components'
-import { themeGet as libThemeGet } from '@styled-system/theme-get'
 
-export const themed = libThemeGet
-
-export const themeGet = (...args) => {
-  console.warn(`
-    themeGet() is deprecated. Please import and use themed().
-  `)
-
-  return themed(...args)
-}
-
-export * from './utils'
 export * from './providers'
-
-export * from './Button'
-export * from './Card'
-export * from './Dropdown'
-export * from './Input'
-export * from './Typography'
-export * from './Grid'
-
-export * from './Button'
 export * from './utils'
+
+export * from './Button'
 export * from './Card'
 export * from './Dropdown'
 export * from './Input'
 export * from './Typography'
 export * from './Tooltip'
+export * from './Grid'
+
 export { styled }
 
 export * from '@ivoryio/kogaio'
