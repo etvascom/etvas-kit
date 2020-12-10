a4d1edf9a9940b531697f756cfe5dc808253a95b
Castravet Radu-Mihai <devicedev@protonmail.com>
2020-12-10 17:45:06 +0200

    feat: add figma icons to glyphs.js

--- src/Icon/Icon.jsx
+++ src/Icon/Icon.jsx
@@ -4,19 +4,30 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import css from '@styled-system/css'
 
-import glyphs from './glyphs.json'
+import glyphs from './glyphs.js'
 import animationSpeeds from '../assets/animationSpeeds'
+import { default as BaseIcon } from '@mdi/react'
 
-export const Icon = ({ name, size, color, rotate }) => {
-  const glyph =
-    typeof name === 'number'
-      ? String.fromCharCode(name)
-      : glyphs[name]
-      ? String.fromCharCode(glyphs[name])
-      : name
-
+export const Icon = ({ name, size, color, rotate, ...props }) => {
+  let glyph
+  if (typeof name === 'number') {
+    glyph = String.fromCharCode(name)
+  } else if (typeof name === 'string') {
+    if (typeof glyphs[name] === 'number') {
+      glyph = String.fromCharCode(glyphs[name])
+    } else if (typeof glyphs[name] === 'string') {
+      return <BaseIcon
+        path={glyphs[name]}
+        size={size}
+        color={color}
+        spin={rotate}
+        {...props}
+      />
+    }
+  }
+  console.warn("You are using old icons!")
   return (
-    <StyledI color={color} size={size} rotate={rotate}>
+    <StyledI color={color} size={size} rotate={rotate} {...props}>
       {glyph}
     </StyledI>
   )
