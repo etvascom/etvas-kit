6238dbe84f682434649df3589a78fa414943d8be
Castravet Radu-Mihai <devicedev@protonmail.com>
2020-12-19 23:26:20 +0200

    feat(glyphs.js) add 3 new icons from figma

--- src/Icon/glyphs.js
+++ src/Icon/glyphs.js
@@ -6,6 +6,7 @@ import {
   mdiAlertCircle,
   mdiAlertCircleOutline,
   mdiArrowLeft,
+  mdiBackburger,
   mdiBookOpenOutline,
   mdiBriefcaseVariant,
   mdiBullhorn,
@@ -30,6 +31,7 @@ import {
   mdiCreditCardRemoveOutline,
   mdiCreditCardSettingsOutline,
   mdiCurrencyEur,
+  mdiDeleteForever,
   mdiDotsHorizontal,
   mdiEye,
   mdiEyeOff,
@@ -39,6 +41,7 @@ import {
   mdiFormatColorFill,
   mdiFormatQuoteClose,
   mdiFormatQuoteOpen,
+  mdiForwardburger,
   mdiFullscreen,
   mdiHandshake,
   mdiHeart,
@@ -143,5 +146,8 @@ export default {
   'loading': mdiLoading,
   'commentQuote': mdiCommentQuote,
   'fileDocumentOutline': mdiFileDocumentOutline,
+  'deleteForever': mdiDeleteForever,
+  'forwardburger': mdiForwardburger,
+  'backburger': mdiBackburger,
   ...oldGlyphs
 }
