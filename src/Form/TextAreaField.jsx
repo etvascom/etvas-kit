f099226802f537a422bc05ac762ec4f90c631a19
Sergiu Elisei <sergiu.elisei@thinslices.com>
2021-06-28 22:09:38 +0300

    fix(TextField) forward the ref to the input component

--- src/Form/TextAreaField.jsx
+++ src/Form/TextAreaField.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import { useField } from 'formik'
 import { TextArea } from '../TextArea'
@@ -6,7 +6,7 @@ import { TextArea } from '../TextArea'
 import { fieldShape } from './shapes'
 import { makeId } from './utils'
 
-export const TextAreaField = props => {
+export const TextAreaField = forwardRef((props, ref) => {
   const [field, meta] = useField(props)
   const id = props.id || makeId('field', props.name || 'textarea')
 
@@ -16,9 +16,10 @@ export const TextAreaField = props => {
       {...field}
       id={id}
       error={meta.touched && meta.error}
+      ref={ref}
     />
   )
-}
+})
 
 TextAreaField.propTypes = {
   ...fieldShape,
