00c3fbca30dfb27f9af67df5ae1d149b5fa5aa87
Sergiu Elisei <sergiu.elisei@thinslices.com>
2021-08-31 09:25:01 +0300

    fix: update storybook config to mute warning

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.(js|jsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+  stories: ['../stories/**/*.stories.@(js|jsx|mdx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  features: {
+    postcss: false
+  }
 }
