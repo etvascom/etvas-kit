dd5eb11709c443ad50868eeed04558bc797cccde
Tudor Hutu <hutu.tudor@yahoo.com>
2022-11-22 13:54:48 +0200

    chore: use esbuild for the start script

--- build.js
+++ build.js
@@ -1,34 +1,62 @@
 const esbuild = require('esbuild')
 const { nodeExternalsPlugin } = require('esbuild-node-externals')
 
-const loader = {
-  '.js': 'jsx',
-  '.jsx': 'jsx',
-  '.woff': 'copy',
-  '.woff2': 'copy',
-  '.svg': 'text'
+const build = () => {
+  const config = {
+    logLevel: 'info',
+    entryPoints: ['src/index.js'],
+    minify: true,
+    bundle: true,
+    loader: {
+      '.js': 'jsx',
+      '.jsx': 'jsx',
+      '.woff': 'copy',
+      '.woff2': 'copy',
+      '.svg': 'text'
+    },
+    plugins: [nodeExternalsPlugin()]
+  }
+
+  esbuild.build({
+    ...config,
+    format: 'esm',
+    outfile: 'es/index.js'
+  })
+
+  esbuild.build({
+    ...config,
+    format: 'cjs',
+    outfile: 'lib/index.js'
+  })
 }
 
-const plugins = [nodeExternalsPlugin()]
+const serve = () =>
+  esbuild
+    .serve(
+      {
+        servedir: 'demo',
+        port: 3000
+      },
+      {
+        entryPoints: ['./demo/src/index.js'],
+        outfile: './demo/dist/index.js',
+        format: 'esm',
+        bundle: true,
+        loader: {
+          '.js': 'jsx',
+          '.jsx': 'jsx',
+          '.woff': 'dataurl',
+          '.woff2': 'dataurl',
+          '.svg': 'dataurl'
+        },
+        plugins: []
+      }
+    )
+    .then(() => console.log('App is live at http://localhost:3000'))
+    .catch(() => process.exit(1))
 
-esbuild.build({
-  logLevel: 'info',
-  entryPoints: ['src/index.js'],
-  bundle: true,
-  format: 'esm',
-  outfile: 'es/index.js',
-  loader,
-  minify: true,
-  plugins
-})
-
-esbuild.build({
-  logLevel: 'info',
-  entryPoints: ['src/index.js'],
-  bundle: true,
-  format: 'cjs',
-  outfile: 'lib/index.js',
-  loader,
-  minify: true,
-  plugins
-})
+if (process.argv.includes('--serve')) {
+  serve()
+} else {
+  build()
+}
