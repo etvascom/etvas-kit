5a76fad610e7f8d6b55964ecd68b9ed7b3508a6a
Radu-Mihai Castravet <devicedev@protonmail.com>
2023-12-27 11:09:05 +0100

    fix(storybook): Fix stories file config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
 
 const config: StorybookConfig = {
-  stories: ['../stories/Typography.stories.tsx'],
+  stories: ['../stories/**/*.stories.@(js|jsx|mdx|tsx|ts)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
