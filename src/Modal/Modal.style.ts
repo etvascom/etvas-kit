ab3b8c7d171f673b7aa4ea6454f5dd2026be39e1
Radu-Mihai Castravet <devicedev@protonmail.com>
2024-11-20 12:44:17 +0100

    feat(Modal): Set width to be percentage to increase consistency

--- src/Modal/Modal.style.ts
+++ src/Modal/Modal.style.ts
@@ -2,14 +2,14 @@ const modalStyles = {
   wrapper: {
     top: '0',
     left: '0',
-    width: '100vw',
+    width: '100%',
     height: '100%',
     position: 'fixed',
     overflowY: 'auto',
     zIndex: 'modal'
   },
   backdrop: {
-    width: '100vw',
+    width: '100%',
     minHeight: '100%',
     top: '0',
     left: '0',
